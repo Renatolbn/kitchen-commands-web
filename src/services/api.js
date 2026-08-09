c6f3dcfd03dbb58162a13ec2245d743564adbdbf
Renatolbn <renatolbn@gmail.com>
2026-08-09 15:46:48 -0300

    feat(auth): integra AuthProvider e roteamento por perfil

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: "http://localhost:3000/api",
   withCredentials: true  // envia cookies automaticamente
 });
 
-export default api
+export default api;
